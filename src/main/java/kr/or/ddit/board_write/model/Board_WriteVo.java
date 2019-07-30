b64fc244a0d1fdaad8ed4da67a799ef676e9bf32
yhs <hansol8600@gmail.com>
2019-07-30 09:13:35 +0900

    Revert "커뮤니티게시판 게시글 작성 및"
    This reverts commit 1d4d6a5cf82ad491fdea0311e4fec2ce6a3891be.

--- src/main/java/kr/or/ddit/board_write/model/Board_WriteVo.java
+++ src/main/java/kr/or/ddit/board_write/model/Board_WriteVo.java
@@ -35,18 +35,6 @@ public class Board_WriteVo {
 	public Board_WriteVo() {
 
 	}
-	
-	
-
-	public Board_WriteVo(int board_id, String user_email, String subject, String content) {
-		super();
-		this.board_id = board_id;
-		this.user_email = user_email;
-		this.subject = subject;
-		this.content = content;
-	}
-
-
 
 	public Board_WriteVo(int write_id, int board_id, String user_email, String subject, String content, Date writedate,
 			int view_cnt, int like_cnt, String del_yn) {
