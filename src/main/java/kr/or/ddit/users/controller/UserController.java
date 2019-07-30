6f71188b632c98f6ad257e7f17a8faf5d4144c1f
kkh <kkh900624@gmail.com>
2019-07-30 17:51:36 +0900

    test

--- src/main/java/kr/or/ddit/users/controller/UserController.java
+++ src/main/java/kr/or/ddit/users/controller/UserController.java
@@ -130,6 +130,7 @@ public class UserController {
 //		notificationSetVo.setNot_cd(not_cd);
 //		notificationSetVo.setNot_chk_fl(not_chk_fl);t
 	
+		
 		int inseertUserNotice = userService.insertUserNotice(notificationSetVo);
 		
 		return "/account/accountSet.user.tiles";
