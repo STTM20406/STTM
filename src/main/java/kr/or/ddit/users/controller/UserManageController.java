f78fba0f9d13314a7b63544bc9488b8338bca0f8
yhs <hansol8600@gmail.com>
2019-08-06 00:42:50 +0900

    지워야함 파일을 이전으로 잘못되돌림

---
+++ src/main/java/kr/or/ddit/users/controller/UserManageController.java
@@ -0,0 +1,28 @@
+package kr.or.ddit.users.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.ddit.paging.model.PageVo;
+import kr.or.ddit.users.model.UserVo;
+import kr.or.ddit.users.service.IUserService;
+
+@Controller
+public class UserManageController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserManageController.class);
+	
+//	@Resource(name = "/userService")
+//	private IUserService userService1;
+	
+
+	
+}
